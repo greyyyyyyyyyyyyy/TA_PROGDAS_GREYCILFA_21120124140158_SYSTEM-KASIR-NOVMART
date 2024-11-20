9680c75f5ce53f7df5201c6a295a83f318b1577a
greyyyyyyyyyyyyy <greycilfa.2006@gmail.com>
2024-11-21 01:38:25 +0700

    sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,30 @@
+<div class="sidebar">
+    <div class="sidebar-header">
+        <h1 class="sidebar-title">ByteGear POS</h1>
+    </div>
+    <div class="menu-container">
+        <h2 class="menu-title">Dashboard</h2>
+        <ul class="menu-list">
+            <li class="menu-item">
+                <a href="tampil_transaksi.php">
+                    <i class="fas fa-exchange-alt"></i> Mutasi Transaksi
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="tambah_transaksi.php">
+                    <i class="fas fa-plus-circle"></i> Tambah Transaksi
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="tampil_barang.php">
+                    <i class="fas fa-box"></i> Daftar Barang
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="laporan_penjualan.php">
+                    <i class="fas fa-chart-bar"></i> Laporan Penjualan
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
